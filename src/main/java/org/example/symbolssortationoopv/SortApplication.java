abb2437a0983c9a0408791b5864f6a10f2a250a4
terentem <86679205+terentem@users.noreply.github.com>
2026-05-22 15:48:56 +0300

    Update SortApplication.java

--- src/main/java/org/example/symbolssortationoopv/SortApplication.java
+++ src/main/java/org/example/symbolssortationoopv/SortApplication.java
@@ -6,27 +6,28 @@ import org.example.symbolssortationoopv.utils.LinkedObjectsCreator;
 import org.example.symbolssortationoopv.utils.Printer;
 import org.example.symbolssortationoopv.utils.StringConvertor;
 
-public class SortApplication {
+public class SortApplication {// TODO : rename into SymbolSorter
 
 
-    public static void startSortation(String strOfSymbols) {
+    public static void startSortation(String strOfSymbols) {//TODO: rename metod into execute, remove static
 
         //1. Convert text(String) into array of char symbols
-        char[] charOfSymbols = StringConvertor.convertStringToCharArr(strOfSymbols);
+        char[] charOfSymbols = StringConvertor.convertStringToCharArr(strOfSymbols);//TODO: move to Main as SymbolSortRequest
 
         //2. Create linked set of objects of Symbol type in heap for every unique symbol from array of char symbols
         LinkedObjectsCreator.createLinkedObjects(charOfSymbols);
-        Symbol lastRefOfLinkedObjects = LinkedObjectsCreator.getLastRefOfLinkedObjects();
+        Symbol lastRefOfLinkedObjects = LinkedObjectsCreator.getLastRefOfLinkedObjects();//TODO: make it not static class field
 
         //3.Create collection of unique Symbol objects
-        SymbolsCollection sb=new SymbolsCollection(lastRefOfLinkedObjects);
+        SymbolsCollection sb=new SymbolsCollection(lastRefOfLinkedObjects);//TODO: rename sb->symbolCollection
         Symbol[] sortedCollection=sb.sortCollection();
-
-        //4.Print this value
-        String text="Unsorted unique symbols:";
-        Printer.printObjectsByLinks(text, lastRefOfLinkedObjects);
-        text="Sorted symbols by frequency:";
-        Printer.printSortedSymbolFrequency(text, sortedCollection);
+       
+        
+       //4.Print this value
+String text="Unsorted unique symbols:"; //TODO: move to Main
+Printer.printObjectsByLinks(text, lastRefOfLinkedObjects);
+text="Sorted symbols by frequency:";
+Printer.printSortedSymbolFrequency(text, sortedCollection);
 
     }
 }
